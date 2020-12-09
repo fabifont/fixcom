9082cf7a410dbab17a81b8731b24c1b09efefe6e
fabifont <fabio.fontana@protonmail.com>
2020-12-09 18:29:27 +0100

    Removed test

--- main.py
+++ main.py
@@ -23,104 +23,97 @@ def get_word(start, char_list):
 
   return word
 
-# # options
-# OPTIONS = 'hnsat:c:e:'
-# LONG_OPTIONS = ['help', 'no-space', 'spaces', 'all', 'type=', 'case=', 'exclude=']
+# options
+OPTIONS = 'hnsat:c:e:'
+LONG_OPTIONS = ['help', 'no-space', 'spaces', 'all', 'type=', 'case=', 'exclude=']
 
-# # get argv
-# argument_list = sys.argv[1:]
+# get argv
+argument_list = sys.argv[1:]
 
-# # no space after comment_type
-# no_space = False
-
-# # fix extra spaces
-# fix_spaces = False
-
-# # format all words in comments
-# format_all = False
-
-# # comment type
-# comment_type = None
-
-# # list of excluded words
-# excluded_list = []
-
-# # upper / Lower case
-# upper = True
-
-# # parsing arguments
-# try:
-#   arguments, values = getopt.getopt(argument_list, OPTIONS, LONG_OPTIONS)
-
-#   # empty command or filename not specified and arg is not 'help'
-#   if len(arguments) == 0 or (len(values) != 1 and (not any('-h' in arg for arg in arguments) and not any('--help' in arg for arg in arguments))):
-#     print('\nEmpty command or missing filename')
-#     print('\nUsage: main.py (-n) -t <comment_type> (-c <upper/lower_case>) (-e <exclude_stringlist>) <filename>')
-#     print('\nUse -h or --help flag to get more info')
-#     sys.exit(2)
-
-#   for current_argument, current_value in arguments:
-#     # case: help
-#     if current_argument in ('-h', '--help'):
-#       print('\nUsage: main.py (-n) -t <comment_type> (-c <upper/lower_case>) (-e <exclude_stringlist>) <filename>')
-
-#       print('\nMandatory arguments:')
-#       print('  -t | --type : comment_type. For example: \'#\' or \'//\'')
-#       print('  filename : filename at the end of the command. For example: \'myfile.py\'')
-#       print('\nExample with only mandaory arguments: python3 main.py -t \'#\' myfile.py')
-
-#       print('\nOptional arguments:')
-#       print('  -h | --help : guideline')
-#       print('  -n | --no-space : format without space after command_type')
-#       print('  -c | --case : uppercase (or u) / lowercase (or l) first char of the word after command_type. Default upper')
-#       print('  -e | --exclude : stringlist of words to exclude from `case` format. For example: \'NULL,OK\'')
-#       print('\nExample: python3 main.py -t \'#\' -c l -e \'NULL,OK\' myfile.py')
-#       sys.exit(0)
-#     # case: no-space
-#     elif current_argument in ('-n', '--no-space'):
-#       no_space = True
-#     # case: fix extra spaces
-#     elif current_argument in ('-s', '--spaces'):
-#       fix_spaces = True
-#     # case: format all words in comments
-#     elif current_argument in ('-s', '--spaces'):
-#       format_all = True
-#     # case: type
-#     elif current_argument in ('-t', '--type'):
-#       comment_type = str(current_value)
-#     elif current_argument in ('-c', '--case'):
-#       # case: lowercase, default uppercase
-#       if(str(current_value) == 'lower' or str(current_value) == 'l'):
-#         upper = False
-#       # case: wrong case value
-#       elif(str(current_value) != 'upper' and str(current_value) != 'u'):
-#         print(
-#             'Wrong case type. Available types are: lower (or l), upper (or u). Default: upper')
-#     # case: exclude
-#     elif current_argument in ('-e', '--exclude'):
-#       # get list of excluded words
-#       excluded_list = current_value.split(',')
-# # wrong command or values
-# except getopt.error as err:
-#   print(err)
-#   print('\nUsage: main.py (-n) -t <comment_type> (-c <upper/lower_case>) (-e <exclude_stringlist>) <filename>')
-#   sys.exit(2)
-
-# # Check mandatory arguments
-# if comment_type == None:
-#   print('\nMissing mandatory comment_type')
-#   print('\nUsage: main.py (-n) -t <comment_type> (-c <upper/lower_case>) (-e <exclude_stringlist>) <filename>')
-#   sys.exit(2)
-
-comment_type = '#'
+# no space after comment_type
 no_space = False
-fix_spaces = True
-format_all = True
-excluded_list = ['NULL']
-upper = False
+
+# fix extra spaces
+fix_spaces = False
+
+# format all words in comments
+format_all = False
+
+# comment type
+comment_type = None
+
+# list of excluded words
+excluded_list = []
+
+# upper / Lower case
+upper = True
+
+# parsing arguments
+try:
+  arguments, values = getopt.getopt(argument_list, OPTIONS, LONG_OPTIONS)
+
+  # empty command or filename not specified and arg is not 'help'
+  if len(arguments) == 0 or (len(values) != 1 and (not any('-h' in arg for arg in arguments) and not any('--help' in arg for arg in arguments))):
+    print('\nEmpty command or missing filename')
+    print('\nUsage: main.py (-n) -t <comment_type> (-c <upper/lower_case>) (-e <exclude_stringlist>) <filename>')
+    print('\nUse -h or --help flag to get more info')
+    sys.exit(2)
+
+  for current_argument, current_value in arguments:
+    # case: help
+    if current_argument in ('-h', '--help'):
+      print('\nUsage: main.py (-n) -t <comment_type> (-c <upper/lower_case>) (-e <exclude_stringlist>) <filename>')
+
+      print('\nMandatory arguments:')
+      print('  -t | --type : comment_type. For example: \'#\' or \'//\'')
+      print('  filename : filename at the end of the command. For example: \'myfile.py\'')
+      print('\nExample with only mandaory arguments: python3 main.py -t \'#\' myfile.py')
+
+      print('\nOptional arguments:')
+      print('  -h | --help : guideline')
+      print('  -n | --no-space : format without space after command_type')
+      print('  -c | --case : uppercase (or u) / lowercase (or l) first char of the word after command_type. Default upper')
+      print('  -e | --exclude : stringlist of words to exclude from `case` format. For example: \'NULL,OK\'')
+      print('\nExample: python3 main.py -t \'#\' -c l -e \'NULL,OK\' myfile.py')
+      sys.exit(0)
+    # case: no-space
+    elif current_argument in ('-n', '--no-space'):
+      no_space = True
+    # case: fix extra spaces
+    elif current_argument in ('-s', '--spaces'):
+      fix_spaces = True
+    # case: format all words in comments
+    elif current_argument in ('-s', '--spaces'):
+      format_all = True
+    # case: type
+    elif current_argument in ('-t', '--type'):
+      comment_type = str(current_value)
+    elif current_argument in ('-c', '--case'):
+      # case: lowercase, default uppercase
+      if(str(current_value) == 'lower' or str(current_value) == 'l'):
+        upper = False
+      # case: wrong case value
+      elif(str(current_value) != 'upper' and str(current_value) != 'u'):
+        print(
+            'Wrong case type. Available types are: lower (or l), upper (or u). Default: upper')
+    # case: exclude
+    elif current_argument in ('-e', '--exclude'):
+      # get list of excluded words
+      excluded_list = current_value.split(',')
+# wrong command or values
+except getopt.error as err:
+  print(err)
+  print('\nUsage: main.py (-n) -t <comment_type> (-c <upper/lower_case>) (-e <exclude_stringlist>) <filename>')
+  sys.exit(2)
+
+# Check mandatory arguments
+if comment_type == None:
+  print('\nMissing mandatory comment_type')
+  print('\nUsage: main.py (-n) -t <comment_type> (-c <upper/lower_case>) (-e <exclude_stringlist>) <filename>')
+  sys.exit(2)
 
 comment_type_size = len(comment_type)
-filename = 'test.txt' #str(values[0])
+filename = str(values[0])
 
 # read the input file
 file = open(filename, 'r')
