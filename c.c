f1fdaee9f602fa74fd88dff7bf65ab435079ed43
fabifont <fabio.fontana@protonmail.com>
2020-12-09 23:09:32 +0100

    Removed test files

--- c.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-// main     function
-int main() {
-    //p poINTS to NULL
-    int *p = NULL;
-    //RETURNS 0
-    return 0;
-}
--- new_c.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-// main function
-int main() {
-    // p points to NULL
-    int *p = NULL;
-    // returns 0
-    return 0;
-}
